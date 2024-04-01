2c44102f87a5637e145bc23fd6e0a9d9e4e138ac
sinhey <sinheyyy@gmail.com>
2024-04-01 23:10:35 +0900

    add required

--- src/component/ContactForm.js
+++ src/component/ContactForm.js
@@ -24,12 +24,12 @@ const ContactForm = () => {
 
                 <Form.Group className="mb-3" controlId="formName">
                     <Form.Label>이름</Form.Label>
-                    <Form.Control type="text" placeholder="이름을 입력해주세요" value={name} onChange={(event) => setName(event.target.value)} />
+                    <Form.Control required type="text" placeholder="이름을 입력해주세요" value={name} onChange={(event) => setName(event.target.value)} />
                 </Form.Group>
 
                 <Form.Group className="mb-3" controlId="formContact">
                     <Form.Label>전화번호</Form.Label>
-                    <Form.Control type="number" placeholder="전화번호를 입력해주세요" value={phoneNumber} onChange={(event) => setPhoneNumber(event.target.value)} />
+                    <Form.Control required type="number" placeholder="전화번호를 입력해주세요" value={phoneNumber} onChange={(event) => setPhoneNumber(event.target.value)} />
                 </Form.Group>
 
                 <Form.Group className="mb-3" controlId="formContact">
